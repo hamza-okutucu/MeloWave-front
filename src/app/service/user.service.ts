500660390a37eb3054a13f656d035047f0e96dad
hamza-okutucu <hamza.okutucu@outlook.com>
2023-12-13 17:04:33 +0100

    update

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -8,7 +8,7 @@ import { User } from '../model/User';
   providedIn: 'root'
 })
 export class UserService {
-  private apiServerBaseUrl = 'http://localhost:8080/api/v1';
+  private apiServerBaseUrl = 'http://51.91.100.173:8080/api/v1';
 
   constructor(private cookieService: CookieService, private http: HttpClient) {}
 
